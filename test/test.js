6cf5353f7665d4df861fe25755682a1bbd86b673
Alejandro Pineda <alejandropi301196@gmail.com>
2020-05-28 20:22:16 +0200

    Mocha ready for test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const lines = ['5 3/n1 1 E/nRFRFRFRF/n3 2 N/nFRRFLLFFRRFLL/n0 3 W/nLLFFFLFLFL']
+
+const program = require('../src/app.js');
+describe('Samples', function() {
+
+
+    //TODO --CALL MAIN FUNCTION
+})
